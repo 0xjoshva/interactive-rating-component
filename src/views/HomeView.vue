1ef4f514803545feb6f2155489665665af25c586
0xjoshva <joshuasteed123@gmail.com>
2022-10-29 18:01:03 +0200

    done done

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,75 +1,112 @@
 <template>
-<section>
-<div class="container" >
-  <div class="form" v-show="monkey === false">
-  <img src="../assets/icon-star.svg" alt="" class="star">
-  <h1>How did we do?</h1>
-  <p>Please let us know how we did with your support request. All the feedback is apprecaited to help us improve our offering!</p>
-  <div class="radio-buttons">
-    <button @click="value = 1" class="radio-btn" :class="{focus : value === 1}">1</button>
-    <button @click="value = 2" class="radio-btn" :class="{focus : value === 2}">2</button>
-    <button @click="value = 3" class="radio-btn" :class="{focus : value === 3}">3</button>
-    <button @click="value = 4" class="radio-btn" :class="{focus : value === 4}">4</button>
-    <button @click="value = 5" class="radio-btn" :class="{focus : value === 5}">5</button>
-  </div>
-</div>
-  <div class="message" v-show="monkey === true">
-    <img src="../assets/illustration-thank-you.svg" alt="">
-    <p id="score">You selected {{ value }} out of 5</p>
-    <h1>Thank you!</h1>
-    <p id="thanks">We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!</p>
-  </div>
-  <button class="submit" @click="monkey = true" :class="{hide : monkey}">SUBMIT</button>
-</div>
-</section>
+  <section>
+    <div class="container">
+      <div class="form" v-show="monkey === false">
+        <img src="../assets/icon-star.svg" alt="" class="star" />
+        <h1>How did we do?</h1>
+        <p>
+          Please let us know how we did with your support request. All the
+          feedback is apprecaited to help us improve our offering!
+        </p>
+        <div class="radio-buttons">
+          <button
+            @click="value = 1"
+            class="radio-btn"
+            :class="{ focus: value === 1 }"
+          >
+            1
+          </button>
+          <button
+            @click="value = 2"
+            class="radio-btn"
+            :class="{ focus: value === 2 }"
+          >
+            2
+          </button>
+          <button
+            @click="value = 3"
+            class="radio-btn"
+            :class="{ focus: value === 3 }"
+          >
+            3
+          </button>
+          <button
+            @click="value = 4"
+            class="radio-btn"
+            :class="{ focus: value === 4 }"
+          >
+            4
+          </button>
+          <button
+            @click="value = 5"
+            class="radio-btn"
+            :class="{ focus: value === 5 }"
+          >
+            5
+          </button>
+        </div>
+      </div>
+      <div class="message" v-show="monkey === true">
+        <img src="../assets/illustration-thank-you.svg" alt="" />
+        <p id="score">You selected {{ value }} out of 5</p>
+        <h1>Thank you!</h1>
+        <p id="thanks">
+          We appreciate you taking the time to give a rating. If you ever need
+          more support, don't hesitate to get in touch!
+        </p>
+      </div>
+      <button class="submit" @click="monkey = true" :class="{ hide: monkey }">
+        SUBMIT
+      </button>
+    </div>
+  </section>
 </template>
 
 <script>
-
 export default {
   data() {
     return {
       monkey: false,
       value: 0,
-  }
-}
-}
+    };
+  },
+};
 </script>
 <style scoped>
-section{
+section {
   width: 100%;
   height: 100vh;
   background: var(--vdblue);
   display: flex;
   align-items: center;
   justify-content: center;
-  font-family: 'Overpass', sans-serif;
+  font-family: "Overpass", sans-serif;
 }
-.container{
+.container {
   width: 25rem;
   height: 25rem;
   border-radius: 26px;
-  background:var(--dblue);
+  background: var(--dblue);
   padding: 2rem;
 }
 
-.star{
+.star {
   background: rgba(128, 128, 128, 0.11);
-  padding: .7rem;
+  padding: 0.7rem;
   border-radius: 50%;
   margin-bottom: 1.5rem;
 }
-h1{
+h1 {
   color: var(--white);
   font-size: 1.8rem;
   font-weight: 700;
 }
-p{
+p {
   color: var(--lgrey);
 }
-.submit{
+.submit {
   width: 100%;
-  display:flex;
+  display: flex;
   justify-content: center;
   align-items: center;
   border-radius: 26px;
@@ -80,60 +117,60 @@ p{
   outline: none;
   line-height: 40px;
 }
-.submit:hover{
-    background: var(--white);
+.submit:hover {
+  background: var(--white);
   color: var(--orange);
 }
-.message{
+.message {
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
 }
-#score{
+#score {
   color: var(--orange);
-   background: rgba(128, 128, 128, 0.11);
-   font-size: 1rem;
-   font-weight: 100;
-   padding: .2rem 0.8rem;
-   border-radius: 26px;
-   margin-top: 1.5rem;
-   margin-bottom: 2rem;
+  background: rgba(128, 128, 128, 0.11);
+  font-size: 1rem;
+  font-weight: 100;
+  padding: 0.2rem 0.8rem;
+  border-radius: 26px;
+  margin-top: 1.5rem;
+  margin-bottom: 2rem;
 }
-#thanks{
+#thanks {
   text-align: center;
-  padding-top: .2rem;
-
+  padding-top: 0.2rem;
 }
-.radio-buttons{
+.radio-buttons {
   display: flex;
   width: 100%;
   justify-content: space-between;
   margin-top: 2rem;
   margin-bottom: 2rem;
 }
-.radio-btn{
+.radio-btn {
   padding: 1rem 1.4rem;
   display: flex;
   justify-content: center;
   align-items: center;
-  border-radius: 50%;  
+  border-radius: 50%;
   background: rgba(128, 128, 128, 0.11);
   color: var(--lgrey);
   line-height: normal;
   border: none;
   outline: none;
 }
-.focus{
-      background: var(--orange);
+.focus {
+  background: var(--orange) !important;
+  color: var(--white) !important;
+}
+.radio-btn:hover {
+  background: var(--mgrey);
   color: var(--white);
 }
-.radio-btn:hover{
-  background: var(--mgrey);
-   color: var(--white);
-}
-.hide{
+.hide {
   display: none;
 }
-.form{}
+.form {
+}
 </style>
